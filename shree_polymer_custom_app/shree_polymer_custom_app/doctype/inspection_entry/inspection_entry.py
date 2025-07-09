e885a4f1ea7079f28ce1b5dd9de9ae705168281e
iyyanarr <iyyanarr@alphaworkz.com>
2025-07-09 03:58:36 +0000

    bug fix

--- shree_polymer_custom_app/shree_polymer_custom_app/doctype/inspection_entry/inspection_entry.py
+++ shree_polymer_custom_app/shree_polymer_custom_app/doctype/inspection_entry/inspection_entry.py
@@ -501,7 +501,7 @@ def make_stock_entry(self):
 					# For rejection entries, use only base weight without shell weight
 					base_wt_per_pi_gms = mould_spec.avg_blank_wtproduct_gms
 					if mould_spec.shell_weight:
-						base_wt_per_pi_gms = base_wt_per_pi_gms + mould_spec.shell_weight
+						base_wt_per_pi_gms = base_wt_per_pi_gms + float(mould_spec.shell_weight)
 					
 					wt_per_pi_gms = base_wt_per_pi_gms
 				else:
